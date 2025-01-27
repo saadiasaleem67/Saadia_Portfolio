1749952cb090c78a9982331fb01ae3b551537142
saadia <saadia.52004@gmail.com>
2025-01-27 16:46:28 +0500

    Updated

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -7,25 +7,26 @@ import { FaGithub } from "react-icons/fa";
 
 const projects = [
   {
-    pic: "/images/diya.png",
-    name: "Diya cosmetics",
-    link: "https://cosmetics-123.vercel.app/",
+    pic: "/images/hajraorganic.png",
+    name: "Hajra Organic",
+    link: "https://hajra-organic.vercel.app/",
     category: "Frontend",
     extra: "",
-    git: "https://github.com/saadiasaleem67/_Cosmetics_123",
+    git: "https://github.com/saadiasaleem67/Hajra_Organic",
     stack: [
       { name: "Nextjs" },
       { name: "Tailwindcss" },
+      { name: "Sanity" },
       { name: "Typescripts" },
     ],
   },
   {
-    pic: "/images/photoelbum.png",
-    name: "Hackathon Photo Elbum",
-    link: "https://cloudinary-photo-album-rho.vercel.app/",
+    pic: "/images/diya.png",
+    name: "Diya Cosmetics",
+    link: "https://cosmetics-123.vercel.app/",
     category: "Frontend",
     extra: "",
-    git: "https://github.com/saadiasaleem67/Cloudinary-photo-album",
+    git: "https://github.com/saadiasaleem67/_Cosmetics_123",
     stack: [
       { name: "Nextjs" },
       { name: "Tailwindcss" },
@@ -39,20 +40,6 @@ const projects = [
     category: "Frontend",
     extra: "In Working",
     git: "/",
-    stack: [
-      { name: "Nextjs" },
-      { name: "Tailwindcss" },
-      { name: "Sanity" },
-      { name: "Typescripts" },
-    ],
-  },
-  {
-    pic: "/images/hajraorganic.png",
-    name: "Hajra Organic",
-    link: "https://hajra-organic.vercel.app/",
-    category: "Frontend",
-    extra: "",
-    git: "https://github.com/saadiasaleem67/Hajra_Organic",
     stack: [
       { name: "Nextjs" },
       { name: "Tailwindcss" },
@@ -88,7 +75,7 @@ const Project = () => {
           transition={{ duration: 0.5, delay: 0.3 }}
         >
           <span className="border border-r-4 border-darkorange "></span>
-          <h3 className="text-3xl capitalize font-semibold">my project</h3>
+          <h3 className="text-3xl capitalize font-semibold">my projects</h3>
         </motion.div>
         {/* project data */}
         <div className="container grid xl:grid-cols-3 lg:grid-cols-2 sm:grid-cols-2 grid-cols-1 place-items-center gap-5 xl:gap-5  mt-16 mb-4">
